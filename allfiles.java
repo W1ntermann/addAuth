c2b536ec8a63d4be12bf7b0c5581f0b8d1cad305
W1ntermann <70312838+W1ntermann@users.noreply.github.com>
2022-06-06 10:05:04 +0300

    Create allfiles.java

---
+++ allfiles.java
@@ -0,0 +1,459 @@
+<parent>
+    <groupId>org.springframework.boot</groupId>
+    <artifactId>spring-boot-starter-parent</artifactId>
+    <version>2.1.9.RELEASE</version>
+</parent>
+
+<dependencies>
+    <dependency>
+        <groupId>org.springframework.boot</groupId>
+        <artifactId>spring-boot-starter-web</artifactId>
+    </dependency>
+    <dependency>
+        <groupId>org.springframework.boot</groupId>
+        <artifactId>spring-boot-starter-security</artifactId>
+    </dependency>
+    <dependency>
+        <groupId>org.springframework.boot</groupId>
+        <artifactId>spring-boot-starter-data-jpa</artifactId>
+    </dependency>
+    <dependency>
+        <groupId>org.postgresql</groupId>
+        <artifactId>postgresql</artifactId>
+        <version>42.2.8</version>
+        <scope>runtime</scope>
+    </dependency>
+    <dependency>
+        <groupId>javax.servlet</groupId>
+        <artifactId>jstl</artifactId>
+        <version>1.2</version>
+    </dependency>
+    <dependency>
+        <groupId>org.apache.tomcat.embed</groupId>
+        <artifactId>tomcat-embed-jasper</artifactId>
+        <version>9.0.27</version>
+    </dependency>
+    <dependency>
+        <groupId>org.springframework.security</groupId>
+        <artifactId>spring-security-taglibs</artifactId>
+        <version>5.2.0.RELEASE</version>
+    </dependency>
+</dependencies>
+
+<build>
+    <plugins>
+        <plugin>
+            <groupId>org.springframework.boot</groupId>
+            <artifactId>spring-boot-maven-plugin</artifactId>
+        </plugin>
+    </plugins>
+</build>
+
+?xml version="1.0" encoding="UTF-8"?>
+<project xmlns="http://maven.apache.org/POM/4.0.0"
+         xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+         xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">
+    <modelVersion>4.0.0</modelVersion>
+
+    <groupId>ark</groupId>
+    <artifactId>spring</artifactId>
+    <version>1.0-SNAPSHOT</version>
+
+    <parent>
+        <groupId>org.springframework.boot</groupId>
+        <artifactId>spring-boot-starter-parent</artifactId>
+        <version>2.1.6.RELEASE</version>
+    </parent>
+
+    <dependencies>
+        <dependency>
+            <groupId>org.springframework.boot</groupId>
+            <artifactId>spring-boot-starter-web</artifactId>
+        </dependency>
+        <dependency>
+            <groupId>org.springframework.boot</groupId>
+            <artifactId>spring-boot-starter-security</artifactId>
+        </dependency>
+        <dependency>
+            <groupId>org.springframework.boot</groupId>
+            <artifactId>spring-boot-starter-data-jpa</artifactId>
+        </dependency>
+        <dependency>
+            <groupId>org.postgresql</groupId>
+            <artifactId>postgresql</artifactId>
+            <scope>runtime</scope>
+        </dependency>
+        <dependency>
+            <groupId>javax.servlet</groupId>
+            <artifactId>jstl</artifactId>
+            <version>1.2</version>
+        </dependency>
+        <dependency>
+            <groupId>org.apache.tomcat.embed</groupId>
+            <artifactId>tomcat-embed-jasper</artifactId>
+            <version>9.0.27</version>
+        </dependency>
+        <dependency>
+            <groupId>org.springframework.security</groupId>
+            <artifactId>spring-security-taglibs</artifactId>
+            <version>5.2.0.RELEASE</version>
+        </dependency>
+
+
+        <dependency>
+            <groupId>org.springframework.boot</groupId>
+            <artifactId>spring-boot-devtools</artifactId>
+        </dependency>
+    </dependencies>
+
+    <properties>
+        <java.version>1.8</java.version>
+    </properties>
+
+    <build>
+        <plugins>
+            <plugin>
+                <groupId>org.springframework.boot</groupId>
+                <artifactId>spring-boot-maven-plugin</artifactId>
+            </plugin>
+        </plugins>
+    </build>
+
+</project>
+
+spring.datasource.url=jdbc:postgresql://localhost/spring
+spring.datasource.username=postgres
+spring.datasource.password=password
+spring.jpa.show-sql=true
+spring.jpa.generate-ddl=false
+spring.jpa.hibernate.ddl-auto=update
+spring.jpa.properties.hibernate.jdbc.lob.non_contextual_creation=true
+
+spring.mvc.view.prefix = /WEB-INF/jsp/
+spring.mvc.view.suffix = .jsp
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
+
+@Query(value = "SELECT nextval(pg_get_serial_sequence('t_user', 'id'))", nativeQuery = true)
+    Long getNextId();
+    
+    public interface RoleRepository extends JpaRepository<Role, Long> {
+}
+
+@Service
+public class UserService implements UserDetailsService {
+    @PersistenceContext
+    private EntityManager em;
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    RoleRepository roleRepository;
+    @Autowired
+    BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+
+        return user;
+    }
+
+    public User findUserById(Long userId) {
+        Optional<User> userFromDb = userRepository.findById(userId);
+        return userFromDb.orElse(new User());
+    }
+
+    public List<User> allUsers() {
+        return userRepository.findAll();
+    }
+
+    public boolean saveUser(User user) {
+        User userFromDB = userRepository.findByUsername(user.getUsername());
+
+        if (userFromDB != null) {
+            return false;
+        }
+
+        user.setRoles(Collections.singleton(new Role(1L, "ROLE_USER")));
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        userRepository.save(user);
+        return true;
+    }
+
+    public boolean deleteUser(Long userId) {
+        if (userRepository.findById(userId).isPresent()) {
+            userRepository.deleteById(userId);
+            return true;
+        }
+        return false;
+    }
+
+    public List<User> usergtList(Long idMin) {
+        return em.createQuery("SELECT u FROM User u WHERE u.id > :paramId", User.class)
+                .setParameter("paramId", idMin).getResultList();
+    }
+}
+
+public boolean saveUser(User user) {
+        User userFromDB = userRepository.findByUsername(user.getUsername());
+
+        if (userFromDB != null) {
+            return false;
+        }
+
+        user.setRoles(Collections.singleton(new Role(1L, "ROLE_USER")));
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        userRepository.save(user);
+        return true;
+    }
+    @Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/news").setViewName("news");
+    }
+}
+
+@Controller
+public class RegistrationController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/registration")
+    public String registration(Model model) {
+        model.addAttribute("userForm", new User());
+
+        return "registration";
+    }
+
+    @PostMapping("/registration")
+    public String addUser(@ModelAttribute("userForm") @Valid User userForm, BindingResult bindingResult, Model model) {
+
+        if (bindingResult.hasErrors()) {
+            return "registration";
+        }
+        if (!userForm.getPassword().equals(userForm.getPasswordConfirm())){
+            model.addAttribute("passwordError", "Пароли не совпадают");
+            return "registration";
+        }
+        if (!userService.saveUser(userForm)){
+            model.addAttribute("usernameError", "Пользователь с таким именем уже существует");
+            return "registration";
+        }
+
+        return "redirect:/";
+    }
+}
+
+@Controller
+public class AdminController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/admin")
+    public String userList(Model model) {
+        model.addAttribute("allUsers", userService.allUsers());
+        return "admin";
+    }
+
+    @PostMapping("/admin")
+    public String  deleteUser(@RequestParam(required = true, defaultValue = "" ) Long userId,
+                              @RequestParam(required = true, defaultValue = "" ) String action,
+                              Model model) {
+        if (action.equals("delete")){
+            userService.deleteUser(userId);
+        }
+        return "redirect:/admin";
+    }
+
+    @GetMapping("/admin/gt/{userId}")
+    public String  gtUser(@PathVariable("userId") Long userId, Model model) {
+        model.addAttribute("allUsers", userService.usergtList(userId));
+        return "admin";
+    }
+}
+
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="sec" uri="http://www.springframework.org/security/tags" %>
+<%@ page language="java" contentType="text/html; charset=utf-8" pageEncoding="utf-8"%>
+
+<!DOCTYPE HTML>
+<html>
+<head>
+  <title>Главная</title>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+  <link rel="stylesheet" type="text/css" href="${contextPath}/resources/css/style.css">
+</head>
+<body>
+<div>
+  <h3>${pageContext.request.userPrincipal.name}</h3>
+  <sec:authorize access="!isAuthenticated()">
+    <h4><a href="/login">Войти</a></h4>
+    <h4><a href="/registration">Зарегистрироваться</a></h4>
+  </sec:authorize>
+  <sec:authorize access="isAuthenticated()">
+    <h4><a href="/logout">Выйти</a></h4>
+  </sec:authorize>
+  <h4><a href="/news">Новости (только пользователь)</a></h4>
+  <h4><a href="/admin">Пользователи (только админ)</a></h4>
+</div>
+</body>
+</html>
+
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ page language="java" contentType="text/html; charset=utf-8" pageEncoding="utf-8"%>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Регистрация</title>
+</head>
+
+<body>
+<div>
+  <form:form method="POST" modelAttribute="userForm">
+    <h2>Регистрация</h2>
+    <div>
+      <form:input type="text" path="username" placeholder="Username"
+                  autofocus="true"></form:input>
+      <form:errors path="username"></form:errors>
+        ${usernameError}
+    </div>
+    <div>
+      <form:input type="password" path="password" placeholder="Password"></form:input>
+    </div>
+    <div>
+      <form:input type="password" path="passwordConfirm"
+                  placeholder="Confirm your password"></form:input>
+      <form:errors path="password"></form:errors>
+        ${passwordError}
+    </div>
+    <button type="submit">Зарегистрироваться</button>
+  </form:form>
+  <a href="/">Главная</a>
+</div>
+</body>
+</html>
+
+<form:form method="POST" modelAttribute="userForm">
+<form:input type="text" path="username" placeholder="Username"></form:input>
+
+<%@ taglib prefix="sec" uri="http://www.springframework.org/security/tags" %>
+<%@ page language="java" contentType="text/html; charset=utf-8" pageEncoding="utf-8"%>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Log in with your account</title>
+</head>
+
+<body>
+<sec:authorize access="isAuthenticated()">
+  <% response.sendRedirect("/"); %>
+</sec:authorize>
+<div>
+  <form method="POST" action="/login">
+    <h2>Вход в систему</h2>
+    <div>
+      <input name="username" type="text" placeholder="Username"
+             autofocus="true"/>
+      <input name="password" type="password" placeholder="Password"/>
+      <button type="submit">Log In</button>
+      <h4><a href="/registration">Зарегистрироваться</a></h4>
+    </div>
+  </form>
+</div>
+
+</body>
+</html>
+
+
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ page language="java" contentType="text/html; charset=utf-8" pageEncoding="utf-8"%>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Log in with your account</title>
+  <link rel="stylesheet" type="text/css" href="${contextPath}/resources/css/style.css">
+</head>
+
+<body>
+<div>
+  <table>
+    <thead>
+    <th>ID</th>
+    <th>UserName</th>
+    <th>Password</th>
+    <th>Roles</th>
+    </thead>
+    <c:forEach items="${allUsers}" var="user">
+      <tr>
+        <td>${user.id}</td>
+        <td>${user.username}</td>
+        <td>${user.password}</td>
+        <td>
+          <c:forEach items="${user.roles}" var="role">${role.name}; </c:forEach>
+        </td>
+        <td>
+          <form action="${pageContext.request.contextPath}/admin" method="post">
+            <input type="hidden" name="userId" value="${user.id}"/>
+            <input type="hidden" name="action" value="delete"/>
+            <button type="submit">Delete</button>
+          </form>
+        </td>
+      </tr>
+    </c:forEach>
+  </table>
+  <a href="/">Главная</a>
+</div>
+</body>
+</html>
+
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ page language="java" contentType="text/html; charset=utf-8" pageEncoding="utf-8"%>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Новости</title>
+</head>
+<body>
+<div>
+    <h2>Новости <br> Только для залогинившихся пользователей.</h2>
+    <a href="/">Главная</a>
+</div>
+</body>
+</html>
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
+
+INSERT INTO public.t_role(id, name)
+  VALUES (1, 'ROLE_USER'), (2, 'ROLE_ADMIN'); 
+  
+  INSERT INTO public.t_user_roles(user_id, roles_id)
+  VALUES (1, 2);
+
+
+
+
+
